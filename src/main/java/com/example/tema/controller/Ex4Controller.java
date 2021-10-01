a0ef53604636521d84f94ea91184d6252e89c3b0
BazaochiDumitru <bazaochi.dumitru@mail.ru>
2021-10-01 09:05:08 +0300

    Ex 4

---
+++ src/main/java/com/example/tema/controller/Ex4Controller.java
@@ -0,0 +1,10 @@
+package com.example.tema.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/ex4")
+public class Ex4Controller {
+    // nu am inteles pana la urma exercitiul
+}
